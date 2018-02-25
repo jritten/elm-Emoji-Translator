b95ea851d47f42919dc9561d6a622443c5c26c9a
Jenna Ritten <jritten@umich.edu>
2018-02-25 14:58:17 -0800

    Initial commit

---
+++ Update.elm
@@ -0,0 +1,16 @@
+module Update exposing (..)
+
+import Model
+
+
+type Msg
+    = SetCurrentText String
+
+
+update : Msg -> Model.Model -> Model.Model
+update msg model =
+    case msg of
+        SetCurrentText newText ->
+            --model
+            { model | currentText = newText }
+
